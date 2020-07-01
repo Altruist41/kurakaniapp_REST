d515edd955121802a2a8688a935491423cb2f4b6
Ajay Shrestha <ajayshrestha1@gmail.com>
2020-07-01 02:37:29 -0600

    Added client side code

---
+++ RestfulChatClient/RestfulChatClient/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace RestfulChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(756, 413);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.textBox4);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Location = new System.Drawing.Point(13, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(731, 401);
+            this.panel2.TabIndex = 4;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(286, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Chat Room";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "User Name";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(398, 165);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Join";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(286, 169);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 1;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(286, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(188, 20);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(669, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Leave";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(668, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 348);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(600, 52);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(732, 329);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 413);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat Client";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ RestfulChatClient/RestfulChatClient/Form1.cs
@@ -0,0 +1,203 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RestfulChatClient
+{
+    public partial class Form1 : Form
+    {
+        string userName = string.Empty;
+        string roomNumber = string.Empty;
+
+        string getTotalUserURL = @"http://localhost:1234/ajay/api/totalusers/get/room/";
+        string getIsRoomValid = @"http://localhost:1234/ajay/api/rooms/get/validity/";
+        string getAllMessages = @"http://localhost:1234/ajay/api/messages/get/room/";
+        string postMessage = @"http://localhost:1234/ajay/api/messages/post";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var timer = new System.Threading.Timer(
+                e => UpdateMessages(),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+        }
+
+        // Join the chat
+        private void button3_Click(object sender, EventArgs e)
+        {            
+            userName = textBox3.Text;
+            roomNumber = textBox4.Text;
+            
+            string getUserCount = HttpGet(getTotalUserURL + roomNumber);
+            dynamic totalUsers = JsonConvert.DeserializeObject(getUserCount.Replace("[", "").Replace("]", ""));
+
+            string getRoomValidity = HttpGet(getIsRoomValid + roomNumber);
+            dynamic roomValidity = JsonConvert.DeserializeObject(getRoomValidity.Replace("[", "").Replace("]", ""));
+
+            if ((int)totalUsers.count < 11)
+            {
+                if ((string)roomValidity.validity == "valid")
+                {
+                    string messageThread = string.Empty;
+
+                    string getMessages = HttpGet(getAllMessages + roomNumber);
+                    dynamic messages = JsonConvert.DeserializeObject(getMessages);
+
+                    int items = ((System.Collections.ICollection)messages).Count;
+
+                    for (int i = 0; i < items; i++)
+                    {
+                        messageThread += (string)messages[i].user + ": " + (string)messages[i].message + Environment.NewLine;
+                    }
+
+                    AppendTextBox(messageThread);
+
+                    panel2.Visible = false;
+                }
+                else
+                {
+                    MessageBox.Show("This room expired, all the old messages are deleted.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sorry, you cannot join this room!");
+            }
+        }
+
+        // Leave the chat
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        // Post message on button click
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = HttpPost(postMessage, "{\"user\":\"" + userName + "\", \"message\":\"" + textBox2.Text + "\", \"room\":\"" + roomNumber + "\"}");
+            textBox2.Text = "";
+        }
+        
+        // Post message on enter key press
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+             if (e.KeyCode == Keys.Enter)
+             {
+                string s = HttpPost(postMessage, "{\"user\":\"" + userName + "\", \"message\":\"" + textBox2.Text + "\", \"room\":\"" + roomNumber + "\"}");
+                textBox2.Text = "";
+            }  
+        }
+
+        // START - Other supporting functions
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+            textBox1.Text = value;
+        }
+
+        void UpdateMessages()
+        {
+            if (userName != string.Empty)
+            {
+                string messageThread = string.Empty;
+
+                string getMessages = HttpGet(getAllMessages + roomNumber);
+                dynamic messages = JsonConvert.DeserializeObject(getMessages);
+
+                int items = ((System.Collections.ICollection)messages).Count;
+
+                for (int i = 0; i < items; i++)
+                {
+                    messageThread += (string)messages[i].user + ": " + (string)messages[i].message + Environment.NewLine;
+                }
+
+                AppendTextBox(messageThread);
+            }
+        }
+
+        public static string HttpPost(string URI, string Parameters)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(URI);
+            //req.Proxy = new System.Net.WebProxy(ProxyString, true);
+            //Adding these, as we're doing a POST
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.Method = "POST";
+            //We need to count how many bytes we're sending. 
+            //Post'ed Faked Forms should be name=value&
+            byte[] bytes = System.Text.Encoding.ASCII.GetBytes(Parameters);
+            req.ContentLength = bytes.Length;
+            System.IO.Stream os = req.GetRequestStream();
+            os.Write(bytes, 0, bytes.Length); //Push it out there
+            os.Close();
+            System.Net.WebResponse resp = req.GetResponse();
+            if (resp == null) return null;
+            System.IO.StreamReader sr =
+                  new System.IO.StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        string HttpGet(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // END - Other supporting functions
+    }
+}
